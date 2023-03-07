d3bd63ba4765a7b2f0b056294fc482eecf8304ba
Javier Torres <jtorres@truora.com>
2023-03-06 21:38:40 -0500

    ping pong

--- main.go
+++ main.go
@@ -23,16 +23,23 @@ func goDotEnvVariable(key string) string {
 	return os.Getenv(key)
 }
 
+func pong(session *discordgo.Session, message *discordgo.MessageCreate) error {
+	_, err := session.ChannelMessageSend(message.ChannelID, "pong")
+	return err
+}
+
 func messageCreate(session *discordgo.Session, message *discordgo.MessageCreate) {
 	if message.Author.ID == session.State.User.ID {
 		return
 	}
 
 	if message.Content == "ping" {
-		_, err := session.ChannelMessageSend(message.ChannelID, "pong")
+		err := pong(session, message)
 		if err != nil {
 			log.Println(err)
 		}
+		log.Println("Ponged")
+		return
 	}
 }
 
